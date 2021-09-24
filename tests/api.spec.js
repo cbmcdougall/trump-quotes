f6fb1438601ae75c82ea63b0c217f1ac9e82645e
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-09-24 11:15:49 +0100

    add initial test setup

---
+++ tests/api.spec.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('', () => {
+    let api;
+    beforeAll(() => {
+        api = app.listen(5000, () => {
+            console.log(`Test API running on port 5000`)
+        });
+    });
+    afterAll((done) => {
+        console.log('Gracefully stopping test server');
+        api.close(done);
+    });
+  
+
+});
