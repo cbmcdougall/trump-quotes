a7948fed649f9ca609576da0edb25f7ce7a7496c
jpxllington <jjdp99@gmail.com>
2021-09-24 15:28:16 +0100

    add quotes to index.html

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,23 @@
 
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-btn.addEventListener('click', getMessage);
+// btn.addEventListener('click', getQuotes);
 
 getQuotes();
+
+function getQuotes(){
+    fetch('http://localhost:3000')
+        .then(r => r.json())
+        // .then(obj =>console.log(obj.quote))
+        .then(obj => appendQuote(obj.quote))
+        .catch(console.warn)
+};
+
+function appendQuote(quote){
+    // const newLi = document.createElement('li');
+    // newLi.textContent = `${quote}`
+    // const quoteList = document.querySelector('ul');
+    // quoteList.append(newLi);
+    document.querySelector('p').textContent = `${quote}`;
+    // console.log(newLi);
+};
