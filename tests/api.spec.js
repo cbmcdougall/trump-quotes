e1670ab6d702549ff34a434c9f7d240ceb5e2b74
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-09-24 13:11:13 +0100

    add GET /:word functionality

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -13,5 +13,21 @@ describe('', () => {
         api.close(done);
     });
   
+    test('GET / returns a random trump quote', (done) => {
+        request(api)
+            .get('/')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    })
+
+    test('GET /:word returns a random trump quote containing the word "word"', (done) => {
+        request(api)
+            .get('/intelligent')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect({quote: "I'm intelligent. Some people would say I'm very, very, very intelligent."})
+            .expect(200, done);
+    })
 
 });
